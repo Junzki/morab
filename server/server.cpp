16b67f17c576a8f3a18ed59fa25dc483537d7ee2
Andrew Junzki <andrew@junzki.me>
2021-02-13 02:06:56 +0800

    WIP: Socket server.

--- server/server.cpp
+++ server/server.cpp
@@ -2,8 +2,23 @@
 #include "morab.h"
 
 
-int
-main(void)
+#pragma comment(lib, "Ws2_32.lib")
+
+// TODO:
+// - Add socket server for windows/*nix platforms.
+//   - Windows: WinSock2 + Windows Select
+//   - *nix: BSD Socket + Select
+//   - `Libevent` is considerable for alternative.
+
+int __cdecl main(void)
 {
-    
+    WSADATA data;
+    if (0 != WSAStartup(MAKEWORD(2, 2), &data))
+    {
+        return 1;
+    }
+
+
+    WSACleanup();
+    return 0;
 }
